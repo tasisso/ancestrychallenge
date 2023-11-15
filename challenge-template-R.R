eb576f978d53895c1f006f2da78d81a5f929dff2
Troy Sisson <tasisson12@gmail.com>
2023-11-15 15:58:20 -0700

    Implement ridge regression

--- challenge-template-R.R
+++ challenge-template-R.R
@@ -1,4 +1,4 @@
-
+install.packages("data.table", repos="cloud.r-project.org")
 library(data.table)
 
 X.train <- as.matrix(data.frame(fread("data/ancestry_train.data")))
@@ -6,7 +6,10 @@ y.train <- as.matrix(data.frame(fread("data/ancestry_train.solution")))
 
 X.test <- as.matrix(data.frame(fread("data/ancestry_test.data")))
 
-
+#Augment training and testing genomatrices
+cbind(a = 0, df)
+X.train <- cbind(V0=1, X.train)
+X.test <- cbind(V0=1, X.test)
 
 #cost function
 cost = function(y.true, y.hat){
@@ -20,9 +23,22 @@ cost(y.train, base)
 
 y.test = matrix(1/3, nrow(X.test), ncol(y.train))
 
+#ridge regression
+beta = function(genotrain, phenotrain, lambda) {
+	SNP.count = ncol(genotrain)
+	imatrix = diag(SNP.count) * lambda
+	tgenotrain = t(genotrain)
+
+	#matrix operations
+	bhats = solve((tgenotrain %*% genotrain) + imatrix) %*% (tgenotrain %*% phenotrain)
+	return(bhats)
+}
+
+#predict y
+result <- X.test %*% beta(X.train, y.train, 0.001)
 
 #save and zip the file
-fwrite(as.data.frame(y.test), file = "predictions.csv",  
+fwrite(as.data.frame(result), file = "predictions.csv",  
 			 sep = " ", quote=FALSE, row.names = F, col.names = F)
 
 system("zip -r predictions.zip predictions.csv")

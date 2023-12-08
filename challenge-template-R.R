46d7a12a4c807cc90cee96eed0ec07542aae4582
Troy Sisson <tasisson12@gmail.com>
2023-12-08 15:37:22 -0700

    "Update for challenge 2"

--- challenge-template-R.R
+++ challenge-template-R.R
@@ -6,11 +6,6 @@ y.train <- as.matrix(data.frame(fread("data/ancestry_train.solution")))
 
 X.test <- as.matrix(data.frame(fread("data/ancestry_test.data")))
 
-#Augment training and testing genomatrices
-cbind(a = 0, df)
-X.train <- cbind(V0=1, X.train)
-X.test <- cbind(V0=1, X.test)
-
 #cost function
 cost = function(y.true, y.hat){
 	mse = -log10(mean((y.true-y.hat)**2)+1e-5)
@@ -29,13 +24,27 @@ beta = function(genotrain, phenotrain, lambda) {
 	imatrix = diag(SNP.count) * lambda
 	tgenotrain = t(genotrain)
 
-	#matrix operations
 	bhats = solve((tgenotrain %*% genotrain) + imatrix) %*% (tgenotrain %*% phenotrain)
 	return(bhats)
 }
 
-#predict y
-result <- X.test %*% beta(X.train, y.train, 0.001)
+
+#bonferroni procedure
+data.train <- cbind(y.train[,1], X.train)
+model <- lm(y ~ ., data.frame(data.train))
+pvalues <- summary(model)$coefficients[,4]
+filterSNPS <- c()
+for (i in 2:length(pvalues)) {
+	if (pvalues[i] <= 0.05/ncol(X.train)) {
+		filterSNPS = append(filterSNPS,i-1)
+	}
+}
+
+#train on selected SNPs
+bfX.train <- cbind(numeric(2000), X.train[,7], X.train[,23], X.train[,55], X.train[,60], X.train[,132], X.train[,178])
+bfX.test <- cbind(numeric(3000), X.test[,7], X.test[,23], X.test[,55], X.test[,60], X.test[,132], X.test[,178])
+
+result <- bfX.test %*% beta(bfX.train, y.train, 0.001)
 
 #save and zip the file
 fwrite(as.data.frame(result), file = "predictions.csv",  
